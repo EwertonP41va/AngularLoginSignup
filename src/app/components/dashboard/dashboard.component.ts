4fcdee5596ca489701d989771ede354f5d2cab7b
EwertonP41va <ewertonpsantos15@gmail.com>
2024-05-12 12:05:34 -0300

    first commit

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
